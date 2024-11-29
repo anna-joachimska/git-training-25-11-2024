a012a40424bb2fbb892cb2a3d4d39db9d56421a9
anna <ania.joachimska@gmail.com>
2024-11-29 10:17:40 +0100

    add new feature amendemnt

--- src/main/java/eu/chrost/git/Feature.java
+++ src/main/java/eu/chrost/git/Feature.java
@@ -3,5 +3,6 @@ package eu.chrost.git;
 public class Feature {
     public static void main(String[] args) {
         System.out.println("my new super feature");
+        System.out.println("my new super feature - amendment");
     }
 }
